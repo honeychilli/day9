8bd0cfeeb48062873a813726c10ebf7331867ab9
honeychilli <arpitpandeyyyy@outlook.com>
2024-10-21 12:12:10 +0000

    second

--- express/app.js
+++ express/app.js
@@ -1,13 +1,27 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 
 app.get('/api/user', (req, res) => {
   const userData = {
     name: 'Arpit',
-    id: 1,
+    
     phoneNumber: '999999999'
   };
+  //send data to mongoDB
+  mongoose.connect('mongodb+srv://arpitpandeyyyy:arpitpandey@deloitte.daxpn.mongodb.net/', { useNewUrlParser: true }).then(() => {
+    console.log('Connected to MongoDB');    
+  }).catch((err) => {
+    console.log(err);
+  })
+  const User = mongoose.model('User', { name: String, id:{
+    type: Number,
+    required: true
+  }, phoneNumber: String });
+  const user = new User(userData);
+  user.save();
   res.json(userData);
+
 });
 
 const port = 8000;
